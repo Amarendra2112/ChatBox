4c281d301f887f1da191970dc63be7ce681b52de
amare <amarendra1197@gmail.com>
2020-07-20 23:53:44 +0530

    MainPage and menu updated

---
+++ app/src/main/java/com/example/chatbox/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.chatbox;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.chatbox.databinding.ActivityMainBinding;
+import com.example.chatbox.menu.CallFragment;
+import com.example.chatbox.menu.ChatFragment;
+import com.example.chatbox.menu.StatusFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        setUpWithViewPager(binding.ViewPager);
+        binding.TabLayout.setupWithViewPager(binding.ViewPager);
+        setSupportActionBar(binding.MainActivityToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.homemenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.SearchMenu)
+        {
+            Toast.makeText(this,"Search",Toast.LENGTH_SHORT).show();
+        }
+
+        else if(item.getItemId() == R.id.MoreMenu)
+        {
+            Toast.makeText(this,"More",Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setUpWithViewPager(ViewPager viewPager)
+    {
+        MainActivity.SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ChatFragment(),"Chat");
+        adapter.addFragment(new StatusFragment(),"Status");
+        adapter.addFragment(new CallFragment(),"Call");
+        viewPager.setAdapter(adapter);
+    }
+
+    //Some function
+
+    private static class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public SectionsPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+}
