7561c649d0787b5aceec27806e6c0555e388f6b3
amare <amarendra1197@gmail.com>
2020-07-30 10:32:41 +0530

    Bottom sheet applied

--- app/src/main/java/com/example/chatbox/setting/SettingProfile.java
+++ app/src/main/java/com/example/chatbox/setting/SettingProfile.java
@@ -3,11 +3,15 @@ package com.example.chatbox.setting;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.chatbox.R;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -19,7 +23,10 @@ public class SettingProfile extends AppCompatActivity {
 
     private FirebaseUser firebaseUser;
     private FirebaseFirestore firebaseFirestore;
-    TextView userName,phone,status;
+    TextView userName,phone;
+    ImageView camera;
+    private BottomSheetDialog bottomSheetDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,7 @@ public class SettingProfile extends AppCompatActivity {
 
         userName = findViewById(R.id.ProfileAboutStatus);
         phone = findViewById(R.id.ProfilePhoneDetail);
+        camera = findViewById(R.id.ProfilePicEdit);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         firebaseFirestore = FirebaseFirestore.getInstance();
@@ -34,6 +42,25 @@ public class SettingProfile extends AppCompatActivity {
         {
             populate();
         }
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetLayout();
+            }
+        });
+    }
+
+    private void showBottomSheetLayout() {
+        View view = getLayoutInflater().inflate(R.layout.bottom_sheet_dialog_box,null);
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(view);
+        bottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                bottomSheetDialog = null;
+            }
+        });
+        bottomSheetDialog.show();
     }
 
     private void populate() {
